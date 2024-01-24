7cec98aeaa9e68719ec5d0cc082765bf1013da02
SHIVAM RISHINARAYAN SINGH <109292385+SHIV000000@users.noreply.github.com>
2024-01-24 11:26:09 +0530

    Update settings.py

--- complaintease/settings.py
+++ complaintease/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gp54#l2fhp2y27hjw&7w4t4j6_f2fql8^v*+bj+p!)lnh$q6=q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
